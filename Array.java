0bab83dcb4bf6673a10ae745e1500b63bb0677c7
eunjaelim <imeunjae361@gmail.com>
2023-04-17 16:01:43 +0900

    배열

---
+++ Array.java
@@ -0,0 +1,15 @@
+package java1;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int i;
+		int[] arrays = new int[] {10,20,30,40};
+		for(i = 0;i<arrays.length;i++) {
+			System.out.println(arrays[i]);
+		}
+
+	}
+	
+	
+}
